0394ff3bf0a528cd1765d317d72308f2e4b967d6
pawaryas <yash.cs1993@mitindore.co.in>
2024-02-17 16:03:57 +0530

    initial code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      Hello Iam Here
+      <Button variant="primary">Click!!</Button>
+    </>
+  );
+}
+
+export default App;
