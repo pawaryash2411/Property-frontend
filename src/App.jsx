6a169d855183e43abfac04c04f448ec38a06050f
Ranjan Lamichhane <studylc29@gmail.com>
2024-02-21 11:58:17 +0545

    property update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-
 import "./App.css";
 import AllRoutes from "./routes/Routes";
 import SideBar from "./Pages/Shared/SideBar";
-import Navbar from "./components/Navbar"
-let App = ()=> {
+import Navbar from "./Components/Navbar";
+let App = () => {
   return (
     <>
-      <Navbar/>
-      <SideBar/>
-      <AllRoutes/>
+      <Navbar />
+      sdkcnsknk
+      <SideBar />
+      <AllRoutes />
     </>
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
